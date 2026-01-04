ed54eda56244da82c49545e219fbfae3845e2070
dnjeh <ehdnj.001@gmail.com>
2026-01-05 01:05:32 +0900

    init

---
+++ app/(main)/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Image from "next/image";
+
+export default function Home() {
+  
+  const initSheet
+
+  localStorage.setItem("moneysheet", JSON.stringify());
+
+  const savedUser = JSON.parse(localStorage.getItem("user"));
+
+  if()
+
+  return new Date(year, month, 0).getDate();
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black">
+        <table>
+          
+        </table>
+      </main>
+    </div>
+  );
+}
